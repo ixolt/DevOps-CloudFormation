518685245f4e22af683ba5a261fd5bc645e16b50
RaduLupan-Q4 <radul@q4inc.com>
2017-04-12 17:52:09 -0400

    no message

--- Q4News/Stage/AWSQ4NewsStage.ps1
+++ Q4News/Stage/AWSQ4NewsStage.ps1
@@ -1,54 +1,36 @@
 ﻿$AWSKeysPath='C:\Users\radul\Google Drive\AWSProdCreds\RaduLupanMicroservices.csv'
 
-Connect-AWSAccount -AWSKeysPath $AWSKeysPath -Region us-west-2 -Profile microservices
+Connect-AWSAccount -AWSKeysPath $AWSKeysPath -Region us-east-1 -Profile microservices
 
 
 ## All CloudFront templates are located in the S3 bucket https://s3.amazonaws.com/q4devops-cf-templates
 
-## 1. Create VPC with 2 x public subnets and 2 x private subnets
+## 1. Create master stacj Q4News-Stage that calls multiple child CloudFormation from S3 bucket
 
 $CFStackParams1=@{'StackName'='Q4News-Stage';`
                  'OnFailure'='ROLLBACK';`
                  'Capability'='CAPABILITY_IAM';`
-                 'TemplateURL'='https://s3.amazonaws.com/q4devops-cf-templates/q4news-vpc-template.json';`
+                 'TemplateURL'='https://s3.amazonaws.com/q4devops-cf-templates/q4news-master.json';`
                 }
 
 
 New-CFNStack @CFStackParams1 `
              -Parameter @( @{ ParameterKey="InstanceTenancy"; ParameterValue="default"}, `
-                           @{ ParameterKey="Environment"; ParameterValue="Stage"})
-
-
-## 2. Create the 2 x FTP servers in each public subnets, create failover DNS recordsin Route 53 for q4app.net domain: ftp-news.q4app.net
-
-
-$CFStackParams2=@{'StackName'='Q4News-Stage-FTP';`
-                 'OnFailure'='ROLLBACK';`
-                 'Capability'='CAPABILITY_IAM';`
-                 'TemplateURL'='https://s3.amazonaws.com/q4devops-cf-templates/q4news-stage-ftp-template.json';`
-                }
-
-
-
-New-CFNStack @CFStackParams2 `
-             -Parameter @( @{ ParameterKey="InstanceType"; ParameterValue="t2.micro"}, `
-                           @{ ParameterKey="VPCID"; ParameterValue="vpc-98bae5ff"}, `
-                           @{ ParameterKey="AppSubnets"; ParameterValue="subnet-c087eda7,subnet-2444c86d"},`
-                           @{ ParameterKey="KeyName"; ParameterValue="MicroservicesOregon"}, `
-                           @{ ParameterKey="APPGroupName"; ParameterValue="FTP"}, `
+                           @{ ParameterKey="Environment"; ParameterValue="stage"}, `
+                           @{ ParameterKey="InstanceType"; ParameterValue="t2.micro"}, `
+                           @{ ParameterKey="KeyName"; ParameterValue="MicroservicesNVirginia"}, `
                            @{ ParameterKey="OctopusEnvironment"; ParameterValue="Microservices Stage"}, `
                            @{ ParameterKey="BootstrapScriptLocation"; ParameterValue="q4news-bootstrap"})
+                           
 
+## 2. Manually install Filezilla Server on both EC2 instances provisioned in step 2. I have not been able to find a way to install FileZilla silently.
 
-## 3. Manually install Filezilla Server on both EC2 instances provisioned in step 2. I have not been able to find a way to install FileZilla silently.
+## 3. Create user Aquire on BOTH FTP servers in FileZilla, point to home folder C:\FTPRoot\Aquire and assign permissions as follows:
+##      3.1 Files: Read, Write, Delete
+##      3.2 Directories: Create, List, +Subdirs
 
-## 4. Create user Aquire on BOTH FTP servers in FileZilla, point to home folder C:\FTPRoot\Aquire and assign permissions as follows:
-##      4.1 Files: Read, Write, Delete
-##      4.2 Directories: Create, List, +Subdirs
+## 4. Configure Filezilla: Edit menu->Settings->Passive mode settings->Use Custom Range: 50000-51000.
 
-## 5. Configure Filezilla: 
-##      5.1 Edit menu->Settings->Passive mode settings->Use Custom Range: 50000-51000.
-##      5.2 External Server IP Address for passive mode transfer, select Use the following IP box and type in the public IP of the FTP server.
 
 
 ## 6. Create RDS PostgresSQL instance: db.m4.large, 100 GB SSD drive, MultiAZ
