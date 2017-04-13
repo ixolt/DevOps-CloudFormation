a70184ef879b3e0361dee580e4ada7a39d546111
RaduLupan-Q4 <radul@q4inc.com>
2017-04-13 14:38:05 -0400

    Deploy Q4News-Stage stack.

--- Q4News/Stage/AWSQ4NewsStage.ps1
+++ Q4News/Stage/AWSQ4NewsStage.ps1
@@ -20,7 +20,10 @@ New-CFNStack @CFStackParams1 `
                            @{ ParameterKey="InstanceType"; ParameterValue="t2.micro"}, `
                            @{ ParameterKey="KeyName"; ParameterValue="MicroservicesNVirginia"}, `
                            @{ ParameterKey="OctopusEnvironment"; ParameterValue="Microservices Stage"}, `
-                           @{ ParameterKey="BootstrapScriptLocation"; ParameterValue="q4news-bootstrap"})
+                           @{ ParameterKey="BootstrapScriptLocation"; ParameterValue="q4news-bootstrap"},`
+                           @{ ParameterKey="DBClass"; ParameterValue="db.m4.large"},`
+                           @{ ParameterKey="DBPassword"; ParameterValue="PleaseChangeMe!"}                           
+                         )
                            
 
 ## 2. Manually install Filezilla Server on both EC2 instances provisioned in step 2. I have not been able to find a way to install FileZilla silently.
@@ -30,28 +33,3 @@ New-CFNStack @CFStackParams1 `
 ##      3.2 Directories: Create, List, +Subdirs
 
 ## 4. Configure Filezilla: Edit menu->Settings->Passive mode settings->Use Custom Range: 50000-51000.
-
-
-
-## 6. Create RDS PostgresSQL instance: db.m4.large, 100 GB SSD drive, MultiAZ
-
-$CFStackParams3=@{'StackName'='Q4News-Stage-RDS';`
-                 'OnFailure'='ROLLBACK';`
-                 'Capability'='CAPABILITY_IAM';`
-                 'TemplateURL'='https://s3.amazonaws.com/q4devops-cf-templates/q4news-rds-template.json';`
-                }
-
-
-
-New-CFNStack @CFStackParams3 `
-             -Parameter @( @{ ParameterKey="VPCID"; ParameterValue="vpc-98bae5ff"}, `
-                           @{ ParameterKey="AppSubnets"; ParameterValue="subnet-c199f3a6,subnet-bb4ac6f2"},`
-                           @{ ParameterKey="DBName"; ParameterValue="newsservicedb"}, `
-                           @{ ParameterKey="DBUsername"; ParameterValue="root"}, `
-                           @{ ParameterKey="DBPassword"; ParameterValue="PleaseChangeMe!"}, `
-                           @{ ParameterKey="DBClass"; ParameterValue="db.m4.large"}, `                           
-                           @{ ParameterKey="DBAllocatedStorage"; ParameterValue="100"})
-
-## Create an EC2 instance in one of the public subnets of Q4News-Stage VPC and install the PostgreSQL pgadmin client 
-## https://www.postgresql.org/ftp/pgadmin3/release/v1.22.2/win32/
-## Run the following SQL scripts on the newsservicedb in order:
