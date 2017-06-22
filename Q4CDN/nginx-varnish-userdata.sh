d04f74b8af2e7d9eeb4828e34ca82b4c3561c0b1
Chris Jones <chris@jonezy.org>
2017-06-21 21:18:14 -0400

    Starting to automate aws ec2 user data

--- Q4CDN/nginx-varnish-userdata.sh
+++ Q4CDN/nginx-varnish-userdata.sh
@@ -2,4 +2,10 @@
 
 sudo apt-get update
 sudo apt-get install -y nginx
+
+sed -i -- 's/:6081/:80/g' /lib/systemd/system/varnish.service
+
+systemctl daemon-reload
+service varnish restart
+
 sudo apt-get install -y varnish
