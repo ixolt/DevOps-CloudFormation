5304df03da8b451f100fc91213b50c61acab74ac
Chris Jones <chris@jonezy.org>
2017-06-21 21:56:43 -0400

    Updated to download test certs

--- Q4CDN/nginx-varnish-userdata.sh
+++ Q4CDN/nginx-varnish-userdata.sh
@@ -6,6 +6,10 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo apt-get -y install varnish
 
+# cert needed for nginx
+sudo curl -o /etc/nginx/ssl/barrick.crt https://raw.githubusercontent.com/q4mobile/DevOps-CloudFormation/master/Q4CDN/barrick.crt
+sudo curl -o /etc/nginx/ssl/barrick.key https://raw.githubusercontent.com/q4mobile/DevOps-CloudFormation/master/Q4CDN/barrick.key
+
 # nginx config
 sudo curl -o /etc/nginx/sites-enabled/default https://raw.githubusercontent.com/q4mobile/DevOps-CloudFormation/master/Q4CDN/default
 
