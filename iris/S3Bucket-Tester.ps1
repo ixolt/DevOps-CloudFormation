4b0bd2dafa85acafc1295ce5dc8cd1b0b9353328
Chris Jones <chrisj@q4inc.com>
2018-02-15 15:06:16 -0500

    Removing test script
    Signed-off-by: Chris Jones <chrisj@q4inc.com>

--- iris/S3Bucket-Tester.ps1
+++
@@ -1,4 +0,0 @@
-$AccessKey = "AKIAI2ZEWVIVKZHGYOHA"
-$AccessSecret = "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"
-
-Write-S3Object -BucketName "iris-templates-dev" -AccessKey $AccessKey -SecretKey $AccessSecret -Key test.txt -File .\iris-templates.json
